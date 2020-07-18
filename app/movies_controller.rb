ccd6b90268dc2d53bb5f5ad0ef43f6bf5023e6d1
ryancdavey1 <ryancdavey1@gmail.com>
2020-07-18 10:59:48 -0600

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -21,6 +21,8 @@ def can_be_created_with_a_hash_of_attributes
       in_theaters: false
   }
   movie = Movie.new(attributes)
+  movie.save
+  movie
 end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
@@ -31,15 +33,16 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
+    m.save
   end
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.all.first
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.all.last
+  Movie.last
 end
 
 def can_get_size_of_the_database
@@ -55,13 +58,13 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.where(:title == "Title", :release_date == 2000, :director == "Me")
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.where(:release_date >= 2002).order(release_date: :desc)
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
